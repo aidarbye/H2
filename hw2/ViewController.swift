7fdbec6c5c09913d05ed04009f1de06555df44c8
Aidar Nurkin <aidar.nurkin@icloud.com>
2023-01-19 18:46:29 +0600

    add color changer

--- hw2/ViewController.swift
+++ hw2/ViewController.swift
@@ -9,11 +9,40 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var mainLabel: UILabel!
+    @IBOutlet var sliderOutlet: UISlider!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        mainLabel.text = ""
+        mainLabel.font = mainLabel.font.withSize(30)
+        mainLabel.textAlignment = .center
+        sliderOutlet.value = 1
+        sliderOutlet.minimumValue = 1
+        sliderOutlet.maximumValue = 5
     }
 
-
+    @IBAction func sliderAction(_ sender: Any) {
+        switch Int(sliderOutlet.value) {
+        case 1:
+            mainLabel.text = "red"
+            view.backgroundColor = .red
+        case 2:
+            mainLabel.text = "yellow"
+            view.backgroundColor = .yellow
+        case 3:
+            mainLabel.text = "brown"
+            view.backgroundColor = .brown
+        case 4:
+            mainLabel.text = "purple"
+            view.backgroundColor = .purple
+        case 5:
+            mainLabel.text = "green"
+            view.backgroundColor = .green
+        default: break
+        }
+        
+    }
+    
 }
 
